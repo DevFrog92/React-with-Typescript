f311c0197f791a838ffd5d004e548779799bc893
DevFrog92 <yongsu1481@gmail.com>
2022-12-01 22:54:40 +0900

    ✨ Implement CountryItem component

---
+++ 03/first-ts-app/src/components/CountryItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { CountryType } from "../App";
+
+type CountryItemPropsType = {
+  countryItem: CountryType;
+};
+
+
+const CountryItem = (props: CountryItemPropsType) => {
+  let item = props.countryItem;
+
+  return (
+    <li
+      className={item.visited ? 'list-group-item active' : 'list-group-item'}>
+      {item.country}
+    </li>
+  )
+}
+
+export default CountryItem
