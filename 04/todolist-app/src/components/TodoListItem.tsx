e61366fab7c1fa2cded58b239c8d254f8e3e32ef
DevFrog92 <yongsu1481@gmail.com>
2022-12-12 21:01:05 +0900

    ✨ Implement TodoListItem component

--- 04/todolist-app/src/components/TodoListItem.tsx
+++ 04/todolist-app/src/components/TodoListItem.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { TodoListItemType } from "../AppContainer";
 
-type Props = {}
+type TodoListItemProps = {
+  todoItem: TodoListItemType;
+  toggleTodo: (no: number) => void;
+  deleteTodo: (no: number) => void;
+}
 
-const TodoListItem = (props: Props) => {
+const TodoListItem = (props: TodoListItemProps) => {
+  let itemClassName = 'list-group-item';
+  if (props.todoItem.done) itemClassName += ' active';
+
   return (
-    <div>TodoListItem</div>
+    <li className={itemClassName}>
+      <span className={props.todoItem.done ? 'todo-done pointer' : 'pointer'}
+        onClick={ () => props.toggleTodo(props.todoItem.no)}
+      >
+        {props.todoItem.todo}
+        {props.todoItem.done ? "DONE" : ""}
+      </span>
+      <span className='float-end badge bg-secondary pointer' onClick={() => props.deleteTodo(props.todoItem.no)}>
+        remove
+      </span>
+    </li>
   )
 }
 
