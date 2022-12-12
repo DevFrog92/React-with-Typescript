44157dd890633047c860223559a5dee5f01e7dd0
DevFrog92 <yongsu1481@gmail.com>
2022-12-12 21:01:18 +0900

    ✨ Implement TodoList component

--- 04/todolist-app/src/components/TodoList.tsx
+++ 04/todolist-app/src/components/TodoList.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { TodoListItemType } from "../AppContainer";
+import TodoListItem from "./TodoListItem";
 
-type Props = {}
+type TodoListProps = {
+  todoList: Array<TodoListItemType>;
+  toggleTodo: (no: number) => void;
+  deleteTodo: (no: number) => void;
+}
 
-const TodoList = (props: Props) => {
+const TodoList = (props: TodoListProps) => {
+  let items = props.todoList.map((item) => {
+    return <TodoListItem key={item.no} todoItem={item} toggleTodo={props.toggleTodo} deleteTodo={ props.deleteTodo}/>
+  })
+
   return (
-    <div>TodoList</div>
+    <div className='row'>
+      {" "}
+      <div className='col'>
+        <ul className='list-group'>{ items }</ul>
+      </div>
+    </div>
   )
 }
 
